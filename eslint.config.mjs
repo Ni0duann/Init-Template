09ef0840b5869642cd27960e3671588bc6f8983f
ni0duann <2014567981@qq.com>
2025-02-23 19:09:31 +0800

    feat: 配置ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "error",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-unused-expressions": "off",
+      "no-unused-labels": "off",
+    },
+  },
 ];
